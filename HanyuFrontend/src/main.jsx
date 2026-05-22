1b398446b3f87dba30d61f81b33c1ea8a9d53782
FWYNEPYE <yenphuong1012.2002@gmail.com>
2026-05-22 16:00:52 +0700

    update

--- HanyuFrontend/src/main.jsx
+++ HanyuFrontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import axios from 'axios'; // 1. Thêm dòng này
+import axios from 'axios'; // 1. Thêm import này
 
+// 2. Thêm đoạn "cảnh sát giao thông" này ngay dưới các dòng import
 axios.interceptors.request.use((config) => {
   if (config.url && config.url.includes('http://localhost:5252')) {
+    // Tự động thay thế localhost bằng link Railway thật
     config.url = config.url.replace('http://localhost:5252', 'https://hanyuapp-production.up.railway.app');
   }
   return config;
