15ee81b9d6e485d5c341ba48b697bc32abf4d675
FWYNEPYE <yenphuong1012.2002@gmail.com>
2026-05-22 15:49:11 +0700

    update

--- HanyuFrontend/src/main.jsx
+++ HanyuFrontend/src/main.jsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import axios from 'axios'; // 1. Thêm dòng này
+
+// 2. Thêm đoạn "cảnh sát giao thông" này ngay dưới dòng import
+axios.interceptors.request.use((config) => {
+  if (config.url && config.url.includes('http://localhost:5252')) {
+    config.url = config.url.replace('http://localhost:5252', 'https://hanyuapp-production.up.railway.app');
+  }
+  return config;
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-{/* bọc login */}
     <GoogleOAuthProvider clientId="458626277443-5tadct3nv33bl5214g7n9uosp519vjq3.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
