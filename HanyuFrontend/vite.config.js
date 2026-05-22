312ff4ca1ef0274375ff28acf38ae678055d3258
FWYNEPYE <yenphuong1012.2002@gmail.com>
2026-05-22 15:27:11 +0700

    update

--- HanyuFrontend/vite.config.js
+++ HanyuFrontend/vite.config.js
@@ -1,16 +1,34 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//     tailwindcss(),
+//   ],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5252',
+//         changeOrigin: true,
+//         secure: false,
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5252',
+        // Chỉ cần để link backend thật của bạn vào đây
+        target: 'https://hanyuapp-production.up.railway.app', 
         changeOrigin: true,
         secure: false,
       }
